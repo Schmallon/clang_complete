e046027eecc2d7a5d09408e59f1b686b3d170950
Matthias Kleine <matthias_kleine@gmx.de>
2011-11-07 23:29:06 +0100

    Use enumerate to maintain index

--- plugin/libclang.py
+++ plugin/libclang.py
@@ -453,18 +453,15 @@ class FindReferencesToOutsideOfSelectionAction(object):
     do_it(translation_unit.cursor, result)
     return result
 
-
 class FindParametersPassedByNonConstReferenceAction(object):
   def find_parameters_passed_by_nonconst_reference(self, translation_unit):
     def get_nonconst_reference_param_indexes(function_decl_cursor):
       result = []
-      index = 0
-      for cursor in function_decl_cursor.get_children():
+      for index, cursor in enumerate(function_decl_cursor.get_children()):
         if cursor.kind == clang.cindex.CursorKind.PARM_DECL:
           if cursor.type.kind in [clang.cindex.TypeKind.LVALUEREFERENCE, clang.cindex.TypeKind.RVALUEREFERENCE]:
             if not cursor.type.get_pointee().is_const_qualified():
               result.append(index)
-        index = index + 1
       return result
 
     def handle_call_expression(result, cursor):
