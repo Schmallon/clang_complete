f1c141cbf463ab75659396c5ddf5676f72c2f498
Matthias Kleine <matthias_kleine@gmx.de>
2012-01-30 19:46:58 +0100

    Highlight member references

--- plugin/libclang.py
+++ plugin/libclang.py
@@ -109,6 +109,7 @@ class VimInterface(object):
         "Virtual method call" : {'group': 'clang_virtual_method_call', 'default' : 'gui=underline guibg=LightGrey'},
         "Virtual method declaration" : {'group': 'clang_virtual_method_declaration', 'default' : 'gui=underline guibg=LightGrey'},
         "Static method declaration" : {'group': 'clang_static_method_declaration', 'default' : 'gui=underline guibg=LightGrey guifg=DarkGreen'},
+        "Member reference" : {'group': 'clang_static_method_declaration', 'default' : 'gui=underline guibg=LightGrey'},
         "Omitted default argument" : {'group': 'clang_omitted_default_argument', 'default': 'ctermbg=6 gui=undercurl guisp=DarkCyan'}}
     self._cached_variable_names = ["g:clang_user_options", "b:clang_user_options", "g:clang_excluded_directories"]
     self._cached_variables = {}
@@ -336,6 +337,7 @@ class ClangPlugin(object):
         ("Virtual method call", FindVirtualMethodCallsAction()),
         ("Virtual method declaration", FindVirtualMethodDeclarationsAction()),
         ("Static method declaration", FindStaticMethodDeclarationsAction()),
+        ("Member reference", FindMemberReferencesAction()),
         ("Omitted default argument", FindOmittedDefaultArgumentsAction())]
 
     for highlight_style, action in styles_and_actions:
