7d58513d8b11450f6a420b714069200f0ec2959b
Matthias Kleine <matthias_kleine@gmx.de>
2013-07-03 16:44:21 +0200

    Print unexpected ranges

--- plugin/test_environment.py
+++ plugin/test_environment.py
@@ -20,6 +20,22 @@ def translation_unit_for(contents):
         [("some_file.cpp", contents)])
 
 
+def mark_unexpected_ranges(source, expected_ranges, actual_ranges):
+    """Doesn't handle overlapping ranges"""
+    missing_ranges = set(actual_ranges) - set(expected_ranges)
+
+    result = ""
+    current_position = 0
+    for range in sorted(missing_ranges):
+        result += source[current_position:range[0]]
+        result += "<UNEXPECTED|"
+        result += source[range[0]: range[1]]
+        result += "|UNEXPECTED>"
+        current_position = range[1]
+
+    return result
+
+
 def assert_ranges_equal(test, source, ranges):
     starts = [a.end() for a in re.finditer("/\\*START\\*/", source)]
     ends = [a.start() for a in re.finditer("/\\*END\\*/", source)]
@@ -28,5 +44,7 @@ def assert_ranges_equal(test, source, ranges):
 
     expected_ranges = zip(starts, ends)
     actual_ranges = [(r.start.offset, r.end.offset) for r in ranges]
+
+    print mark_unexpected_ranges(source, expected_ranges, actual_ranges)
 
     test.assertEqual(set(expected_ranges), set(actual_ranges))
