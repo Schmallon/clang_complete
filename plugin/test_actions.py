de704a7fe744b220eae80b2c5fc71a1bb9971e19
Matthias Kleine <matthias_kleine@gmx.de>
2013-07-03 15:55:40 +0200

    Extract assert_function_finds_marked_ranges

--- plugin/test_actions.py
+++ plugin/test_actions.py
@@ -5,9 +5,18 @@ from test_environment import translation_unit_for, assert_ranges_equal
 
 class TestActions(unittest.TestCase):
 
+    def assert_function_finds_marked_ranges(self, function, source):
+        with translation_unit_for(source) as translation_unit:
+            assert_ranges_equal(
+                self,
+                source,
+                function(translation_unit))
+
     def test_find_virtual_method_calls(self):
 
-        source = """
+        self.assert_function_finds_marked_ranges(
+            actions.find_virtual_method_declarations,
+            """
             class Foo
             {
             public:
@@ -22,27 +31,16 @@ class TestActions(unittest.TestCase):
 
               foo.virtual_method();
               foo.non_virtual_method();
-            }"""
-
-        with translation_unit_for(source) as translation_unit:
-            assert_ranges_equal(
-                self,
-                source,
-                actions.find_virtual_method_declarations(translation_unit))
+            }""")
 
     def test_find_omitted_default_arguments(self):
-        source = """
-
+        self.assert_function_finds_marked_ranges(
+            actions.find_omitted_default_arguments,
+            """
             void function_with_default_arguments(int x, int y = 0);
 
             void test()
             {
               /*START*/function_with_default_arguments(5)/*END*/;
               function_with_default_arguments(5, 6);
-            }"""
-
-        with translation_unit_for(source) as translation_unit:
-            assert_ranges_equal(
-                self,
-                source,
-                actions.find_omitted_default_arguments(translation_unit))
+            }""")
