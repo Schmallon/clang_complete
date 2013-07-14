46d6ab07612f05317b827567ed7e47cbc0f335d2
Matthias Kleine <matthias_kleine@gmx.de>
2013-07-14 21:52:18 +0200

    Extract interesting_range_collector

--- plugin/highlighting.py
+++ plugin/highlighting.py
@@ -14,12 +14,52 @@ def export_and_highlight_range_if_in_current_file(editor, range, highlight_style
     highlight_range_if_in_current_file(editor, exported_range, highlight_style)
 
 
+def interesting_range_collector(styles_and_actions):
+
+    quick_fix_list_generator = QuickFixListGenerator()
+
+    def do_it(translation_unit):
+
+        class MemoizedTranslationUnit(object):
+            def __init__(self, translation_unit):
+                self.cursor = translation_unit.cursor
+                self.spelling = translation_unit.spelling
+                self.diagnostics = translation_unit.diagnostics
+        memoized_translation_unit = MemoizedTranslationUnit(translation_unit)
+
+        def collect_ranges():
+            for highlight_style, action in styles_and_actions:
+                ranges = action(memoized_translation_unit)
+                for range in ranges:
+                    yield ExportedRange.from_clang_range(range), highlight_style
+
+        diagnostics = quick_fix_list_generator.get_quick_fix_list(translation_unit)
+
+        return diagnostics, list(collect_ranges())
+
+    return do_it
+
+
 class InterestingRangeHighlighter(object):
     def __init__(self, current_translation_unit_access, editor):
-        current_translation_unit_access.add_listener(self._process_translation_unit)
-        self._results = ReplacingSingleElementQueue()
         self._editor = editor
-        self._quick_fix_list_generator = QuickFixListGenerator(self._editor)
+        self._results = ReplacingSingleElementQueue()
+
+        collector = interesting_range_collector(self._styles_and_actions())
+
+        def publish_result(translation_unit):
+            self._results.put(collector(translation_unit))
+
+        current_translation_unit_access.add_listener(publish_result)
+
+    def tick(self):
+        try:
+            diagnostics, ranges = self._results.get_nowait()
+            self._editor.display_diagnostics(diagnostics)
+            self._clear_interesting_ranges()
+            self._highlight_interesting_ranges(ranges)
+        except Queue.Empty:
+            pass
 
     def _styles_and_actions(self):
         return [
@@ -36,38 +76,6 @@ class InterestingRangeHighlighter(object):
         for highlight_style, action in self._styles_and_actions():
             self._editor.clear_highlights(highlight_style)
 
-    def _process_translation_unit(self, translation_unit):
-        self._results.put(self._collect_interesting_ranges(translation_unit))
-
-    def tick(self):
-        try:
-            diagnostics, ranges = self._results.get_nowait()
-            self._editor.display_diagnostics(diagnostics)
-            self._clear_interesting_ranges()
-            self._highlight_interesting_ranges(ranges)
-        except Queue.Empty:
-            pass
-
-    def _collect_interesting_ranges(self, translation_unit):
-
-        class MemoizedTranslationUnit(object):
-            def __init__(self, translation_unit):
-                self.cursor = translation_unit.cursor
-                self.spelling = translation_unit.spelling
-                self.diagnostics = translation_unit.diagnostics
-        memoized_translation_unit = MemoizedTranslationUnit(translation_unit)
-
-        def collect_ranges():
-            for highlight_style, action in self._styles_and_actions():
-                ranges = action(memoized_translation_unit)
-                for range in ranges:
-                    yield ExportedRange.from_clang_range(range), highlight_style
-
-        diagnostics = self._quick_fix_list_generator.get_quick_fix_list(translation_unit)
-
-        return diagnostics, list(collect_ranges())
-
-
     def _highlight_interesting_ranges(self, ranges):
         for range, highlight_style in ranges:
             highlight_range_if_in_current_file(self._editor, range, highlight_style)
@@ -76,16 +84,12 @@ class InterestingRangeHighlighter(object):
 
 class QuickFixListGenerator(object):
 
-    def __init__(self, editor):
-        self._editor = editor
-
     def _get_quick_fix(self, diagnostic):
         # Some diagnostics have no file, e.g. "too many errors emitted, stopping now"
         if diagnostic.location.file:
             file_name = diagnostic.location.file.name
         else:
             "hack: report errors without files. should nevertheless be in quick_fix list"
-            self._editor.display_message(diagnostic.spelling)
             file_name = ""
 
         if diagnostic.severity == diagnostic.Ignored:
