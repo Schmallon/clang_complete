5bdd058473cb7f9b4fe1f6e30b05fce13a38ee74
Matthias Kleine <matthias_kleine@gmx.de>
2013-07-26 13:57:30 +0200

    Read clang library path from file

--- plugin/configure_clang.py
+++ plugin/configure_clang.py
@@ -2,7 +2,7 @@ import clang.cindex
 
 
 def configure():
-    clang_path = "/Users/mkl/projects/llvm/ninja/lib"
+    clang_path = open(".clang_path", "r").read().strip()
 
     if not clang.cindex.Config.library_path:
         clang.cindex.Config.set_library_path(clang_path)
